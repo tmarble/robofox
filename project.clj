06b7b2f34e00685e70d12e51fc9799c0cac0a41e
Tom Marble <tmarble@info9.net>
2015-08-03 18:47:02 -0500

    updated to clj-webdriver 0.7.5

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject robofox "0.2.0"
+(defproject robofox "0.2.1"
   :description "Demo of driving Firefox with clj-webdriver"
   :url "https://github.com/tmarble/robofox"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
-                 [net.info9/clj-webdriver "0.7.3"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [net.info9/clj-webdriver "0.7.5"]]
   :main robofox.core )
